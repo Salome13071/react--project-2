776843e90099a6e1c5386389e92b1f113c5b6404
Salome Karseladze <saly13071@gmail.com>
2024-12-17 11:29:19 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2> Core Concepts </h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import reactImg from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+export function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom representing React concepts" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+export default Header;
