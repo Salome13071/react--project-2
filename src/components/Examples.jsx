2e6ea6348a48ae3e4f1a65b5190954f559c23742
Salome Karseladze <saly13071@gmail.com>
2024-12-20 13:31:05 +0400

    last version

---
+++ src/components/Examples.jsx
@@ -0,0 +1,61 @@
+import TabButton from "./TabButton";
+import { EXAMPLES } from "../data";
+import { useState } from "react";
+import Section from "./Section";
+import Tabs from "./Tabs";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+  return (
+    <Section title="Examples" id="examples" className="ddd">
+      <Tabs
+        buttonsContainer="menu"
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleClick("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleClick("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      />
+      {!selectedTopic ? <p>Please select a topic</p> : null}
+      {selectedTopic ? (
+        <div id="tab-content">
+          {" "}
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+      ) : null}
+    </Section>
+  );
+}
